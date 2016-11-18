68f437afdae996ddc92b235854f826d81938bb69
cody constine <cody.constine@colorado.edu>
2016-11-17 17:39:40 -0700

    Added handler functions

--- dfc.c
+++ dfc.c
@@ -19,9 +19,9 @@
 #define MAXBUFSIZE 1024
 
 //thread handlers
-int listHandler(int);
-int getHandler(int, char[]);
-int putHandler(int, char[]);
+int listHandler();
+int getHandler(char*);
+int putHandler(char*);
 
 //file parser functions
 char * getUser();
@@ -55,21 +55,40 @@ int main(int argc, char * argv[])
 		else if( strncmp(pch, "put", 3) == 0)
 		{
             pch = strtok (NULL, " ");
-            printf("PUT command recieved: %s\n", pch);
+            int responce = putHandler(pch);
+            printf("Put Handler returned: %d\n", responce);
 		}
 		else if(strncmp(pch, "get", 3) == 0)
 		{
             pch = strtok (NULL, " ");
-            printf("GET command recieved: %s\n", pch);
+            int responce = getHandler(pch);
+            printf("Get Handler returned: %d\n", responce);
 		}
 		else if(strncmp(pch, "list", 2) == 0)
 		{
-            pch = strtok (NULL, " ");
-            printf("LIST command recieved: %s\n", pch);
+            int responce = listHandler();
+            printf("List Handler returned: %d\n", responce);
 		}
 		else
 			printf("%s is an invalid command.\n", command);
 
 	}
+    return 0;
+}
+
+
+int listHandler()
+{
+    printf("LIST command recieved.\n");
+    return 0;
+}
+int getHandler(char * file)
+{
+    printf("GET command recieved: %s\n", file);
+    return 0;
+}
+int putHandler(char * file)
+{
+    printf("PUT command recieved: %s\n", file);
     return 0;
 }
