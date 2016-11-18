4cb54874e934dfe7381843a69eeb1898bbd78312
cody constine <cody.constine@colorado.edu>
2016-11-18 11:26:55 -0700

    Added stuff to support sockets correctly

--- Makefile
+++ Makefile
@@ -4,6 +4,6 @@ client: dfc.c
 	gcc -Wall -o dfc dfc.c -lpthread
 
 server: dfs.c
-	gcc -Wall -o dfs dfs.c
+	gcc -Wall -o dfs dfs.c -lpthread
 clean:
 	rm dfs dfc
--- dfc.c
+++ dfc.c
@@ -31,13 +31,14 @@ char * getServerPort(int);
 
 int main(int argc, char * argv[])
 {
+
+
     if(argc != 2)
     {
         printf("Correct Usage: ./dfc <dfc.conf>\n");
     }
-
-	char *newline = NULL;  //to remove new
 	char command[MAXBUFSIZE];
+    char * newline;
 	char * pch;
 	while(1)
 	{
@@ -89,6 +90,34 @@ int getHandler(char * file)
 }
 int putHandler(char * file)
 {
+    int sockfd, portno;
+
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    //char buffer[256];
+    portno = 10000;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        printf("ERROR opening socket\n");
+    server = gethostbyname("127.0.0.1");
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+        printf("ERROR connecting\n");
     printf("PUT command recieved: %s\n", file);
+    char * message = (char*)malloc(strlen("put ")+strlen(file)+1);
+    strcpy(message, "put ");
+    strcat(message, file);
+    printf("Sending file message: %s\n", message);
+    send(sockfd, message, strlen(message), 0);
     return 0;
 }
